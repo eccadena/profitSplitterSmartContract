9268d796e8e02f0da7c472f9889bbe3f544221f0
eccadena <eccadena@gmail.com>
2019-12-29 21:31:58 -0600

    Level 2 Tiered Profit Splitter Contract

--- AssociateProfitSplitter.sol
+++ AssociateProfitSplitter.sol
@@ -1,65 +1,32 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "pragma solidity ^0.5.0;\n",
-    "\n",
-    "// lvl 1: equal split\n",
-    "contract AssociateProfitSplitter {\n",
-    "    address payable employee_one; \n",
-    "    address payable employee_two;\n",
-    "    address payable employee_three;\n",
-    "    \n",
-    "    constructor(address payable _one, address payable _two, address payable _three) public {\n",
-    "        employee_one = _one;\n",
-    "        employee_two = _two;\n",
-    "        employee_three = _three;\n",
-    "    }\n",
-    "\n",
-    "    function balance() public view returns(uint) {\n",
-    "        return address(this).balance;\n",
-    "    }\n",
-    "\n",
-    "    function deposit() public payable {\n",
-    "        uint amount = (msg.value / 3) ; \n",
-    "\n",
-    "        employee_one.transfer(amount);\n",
-    "        employee_two.transfer(amount);\n",
-    "        employee_three.transfer(amount);\n",
-    "        \n",
-    "        msg.sender.transfer(msg.value - amount*3);\n",
-    "    }\n",
-    "    \n",
-    "    function() external payable {\n",
-    "        deposit();\n",
-    "    }\n",
-    "}"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
+pragma solidity ^0.5.0;
+
+// lvl 1: equal split
+contract AssociateProfitSplitter {
+    address payable employee_one; 
+    address payable employee_two;
+    address payable employee_three;
+    
+    constructor(address payable _one, address payable _two, address payable _three) public {
+        employee_one = _one;
+        employee_two = _two;
+        employee_three = _three;
+    }
+
+    function balance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function deposit() public payable {
+        uint amount = (msg.value / 3) ; 
+
+        employee_one.transfer(amount);
+        employee_two.transfer(amount);
+        employee_three.transfer(amount);
+        
+        msg.sender.transfer(msg.value - amount*3);
+    }
+    
+    function() external payable {
+        deposit();
+    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # profitSplitterSmartContract
 Using solidity, remix, metamask and more to create smart contracts in solidity.
+
+## Associate Profit Splitter Smart Contract
+
+This contract is a very simple smart contract that allows an entity to pay 3 employees equal amounts to their addresses.
+
+These amounts may result in a remainder that, in total, would not equal to the total amount to pay out to the employees. This contract will send this amount back to the caller of the smart contract.
+
+## Tired Profit Splitter Smart Contract
+
+This contract takes the previous contract to the next level. 
+
+This contract allows the caller to send a percentage of an amount (via points out of a percentage of 100) to their employee's addresses. 
